17d44de53eeaa5ebb8cc4b73055f44cd0ed0b7fe
xiexiaolong <601318482@qq.com>
2016-06-19 18:36:13 +0800

    Initial Commit

---
+++ AVFoundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundation
+//
+//  Created by xiexiaolong on 16/6/19.
+//  Copyright © 2016年 xiexiaolong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
